494db3df4494fa826f22551633ff1e44ec22fb38
Tamas Soos <Tamas.Soos@goto.com>
2023-02-18 03:21:05 +0100

    Add responsive design with dark mode switcher

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,15 @@
-const IndexPage = () => <h1>Hello Next.js 👋</h1>;
+import { Container, Flex } from "@chakra-ui/react";
+import Cart from "../src/sections/cart";
+import Details from "../src/sections/details";
+
+const IndexPage = () => (
+  <Container maxW="container.xl" p={0}>
+    {/* responsive design with array and object syntax */}
+    <Flex h={{ base: "auto", md: "100vh" }} py={[0, 10, 20]} direction={{ base: "column-reverse", md: "row" }}>
+      <Details />
+      <Cart />
+    </Flex>
+  </Container>
+);
 
 export default IndexPage;
