14c08373d5f46c83af5f8721cec0736923e9f91d
Tamas Soos <Tamas.Soos@goto.com>
2023-02-19 03:29:57 +0100

    Add primary variant to Button

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,4 +1,5 @@
 import { extendTheme, theme as base, type ThemeConfig, ComponentStyleConfig, withDefaultColorScheme, withDefaultVariant } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
 
 const inputSelectStyles: ComponentStyleConfig = {
   // overriding the "Input" and "Select" components' default style.
@@ -23,6 +24,13 @@ const inputSelectStyles: ComponentStyleConfig = {
         borderRadius: "none",
       },
     },
+  },
+};
+
+const brandRing = {
+  _focus: {
+    ring: 2,
+    ringColor: "base.500",
   },
 };
 
@@ -64,16 +72,29 @@ const config: ThemeConfig & MyCustomConfig = {
           /* a Checkbox has 4 parts: Control, Icon, Container and Label:
 					https://chakra-ui.com/docs/components/checkbox/theming */
           borderRadius: "radii.none", // same as "none".
-          _focus: {
-            ring: 2,
-            ringColor: "base.500",
-          },
+          ...brandRing,
         },
       },
     },
-    Button: {
+    /* Button: { // this also works...
       baseStyle: {
         borderRadius: 0, // same as "none".
+      },
+    }, */
+    Button: {
+      variants: {
+        primary: (props) => ({
+          rounded: "none",
+          ...brandRing,
+          color: mode("white", "gray.800")(props),
+          backgroundColor: mode("brand.500", "brand.200")(props),
+          _hover: {
+            backgroundColor: mode("brand.600", "brand.300")(props),
+          },
+          _active: {
+            backgroundColor: mode("brand.700", "brand.400")(props),
+          },
+        }),
       },
     },
   },
